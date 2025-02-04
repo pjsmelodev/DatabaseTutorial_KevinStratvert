fb7baee29915359d0699ea527e11a86d614c68f0
pjsm <pjsm.lesi@gmail.com>
2025-02-04 20:24:57 +0000

    Corrected sql file name

--- SQLQuery_KevinStratvert_SelectAll_And_Or.sql
+++ SQLQuery_KevinStratvert_SelectAll_And_Or.sql
@@ -1,3 +1,3 @@
 SELECT *
 FROM KCC.dbo.Customers
-WHERE CustomerName = 'Tres Delicious' AND Country = 'United States' Or Country = 'France'
+WHERE CustomerName = 'Tres Delicious' AND (Country = 'United States' Or Country = 'France')
